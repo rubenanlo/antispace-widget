2796bef3f8dabac42bd4e84b0c11ec5529759e67
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-10-04 18:25:02 +0200

    setup: uninstall all packages for image optimization and some tailwind unused plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import { fontFamily } from "tailwindcss/defaultTheme";
-const aspectRatio = require("@tailwindcss/aspect-ratio");
 const scrollbarHide = require("tailwind-scrollbar-hide");
-const forms = require("@tailwindcss/forms");
 
 module.exports = {
   content: [
@@ -26,5 +24,5 @@ module.exports = {
       },
     },
   },
-  plugins: [aspectRatio, scrollbarHide, forms],
+  plugins: [scrollbarHide],
 };
