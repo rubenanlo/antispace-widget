947ecf720ac53c5e2869dbaad3b967f0ce0091a9
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-10-09 17:19:58 +0200

    feature: allowed caching and added bundle analysis

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["geist"],
+  compress: true, // Enable compression
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);
